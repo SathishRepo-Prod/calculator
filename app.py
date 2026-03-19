4e8a792d281ba4cf66dcb27e1308b551a8de9de2
sathish <sathish@gmail.com>
2026-03-19 14:24:44 +0530

    calculator

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def calculator():
+    result = None
+
+    if request.method == "POST":
+        try:
+            a = float(request.form["a"])
+            b = float(request.form["b"])
+            op = request.form["operation"]
+
+            if op == "add":
+                result = a + b
+            elif op == "sub":
+                result = a - b
+            elif op == "mul":
+                result = a * b
+            elif op == "div":
+                result = "Error" if b == 0 else a / b
+
+        except:
+            result = "Invalid Input"
+
+    return render_template("index.html", result=result)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
